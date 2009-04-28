80500291ba2a097724385929d977827d6fb77f67
Christopher Corley <cscorley@una.edu>
2009-04-28 03:18:32 +0000

    Stackers

--- 5-Stacker/stacker.adb
+++ 5-Stacker/stacker.adb
@@ -24,81 +24,104 @@ procedure Stacker is
 	end record;
 
 	ar : array (1..5) of StackItem;
+	procedure createNewStack (S: in out StackItem) is
+		input : Character;
+	begin
+		Put("Enter 1 Character to name the stack: ");
+		Get(input); -- this really needs to be a Get_Line
+		S.Name := input;
+		Reset(S.Item);
+	exception
+		when DATA_ERROR | CONSTRAINT_ERROR =>
+			Put_Line("[!] Please enter only 1 character.");
+			createNewStack(S); -- hopefully they get it right this time.
+	end createNewStack;
+
+	procedure pushValue (S: in out StackItem) is
+		input : Integer;
+	begin
+		Put("Enter the integer to be pushed onto ");
+		Put(S.Name);
+		Put(": ");
+		Get(input);
+		Push(S.Item, input);
+	end pushValue;
+
+	procedure popValue (S: in out StackItem) is
+		output : Integer;
+	begin
+		Put("The integer popped from ");
+		Put(S.Name);
+		Put(": ");
+		Pop(S.Item, output);
+		Put(output);
+	end popValue;
+
+	procedure emptyCheck (S: in out StackItem) is
+	begin
+		if (IsEmpty(S.Item)) then
+			Put(S.Name);
+			Put(" is empty.");
+		else
+			Put(S.Name);
+			Put(" is not empty.");
+		end if;
+	end emptyCheck;
+
+	procedure displaySize (S: in out StackItem) is
+	begin
+		Put(S.Name);
+		Put(" has ");
+		Put(StackSize(S.Item));
+		Put(" integers.");
+	end;
 
 	procedure stackMenu(S: in out StackItem) is
-		option : Natural := 12;
+		option : Natural := 6;
 	begin
 		BossLoop:
 		loop
 			WorkerLoop:
 			loop
 				New_Line;
-				Put("What would you like to do to");
-				Put(S.Name);
-				Put_Line("? (0 to exit)");
-				Put_Line(" 1. Create new stack");
-				Put_Line(" 2. Push integer to stack");
-				Put_Line(" 3. Pop integer from stack");
-				Put_Line(" 4. Check for empty stack");
-				Put_Line(" 5. Check for full stack");
-				Put("Your selection number: ");
-				begin
-					Get(option);
-				exception
-					when CONSTRAINT_ERROR => exit WorkerLoop;
-				end;
-				exit BossLoop when option = 0;
+				if S.Name /= ' ' then
+					Put_Line("What would you like to do to " & S.Name & "?");
+					Put_Line(" 1. Create/Name new stack");
+					Put_Line(" 2. Push integer to stack");
+					Put_Line(" 3. Pop integer from stack");
+					Put_Line(" 4. Check for empty stack");
+					Put_Line(" 5. Check for full stack");
+					Put_Line(" 0. Exit to main menu");
+					Put("Your selection number: ");
+					begin
+						Get(option); -- this really needs to be a Get_Line
+						exit BossLoop when option = 0;
+					exception
+						when CONSTRAINT_ERROR => exit WorkerLoop;
+					end;
+				else
+					option := 1; -- force to name the new stack first
+				end if;
 				New_Line;
 				case option is
 					when 1 =>
-						declare
-							input : Character;
-						begin
-							Put("Enter 1 Character to name the stack: ");
-							Get(input);
-							-- failure
-							S.Name := input;
-							Reset(S.Item);
-						end;
+						createNewStack(S);
 					when 2 =>
-						declare
-							input : Integer;
-						begin
-							Put("Enter the integer to be pushed onto ");
-							Put(S.Name);
-							Put(": ");
-							Get(input);
-							Push(S.Item, input);
-						end;
+						pushValue(S);
 					when 3 =>
-						declare
-							output : Integer;
-						begin
-							Put("The integer popped from");
-							Put(S.Name);
-							Put(": ");
-							Pop(S.Item, output);
-							Put(output);
-						end;
+						popValue(S);
 					when 4 =>
-						if (IsEmpty(S.Item)) then
-							Put(S.Name);
-							Put(" is empty.");
-						else
-							Put(S.Name);
-							Put(" is not empty.");
-						end if;
+						emptyCheck(S);
 					when 5 =>
-						Put(S.Name);
-						Put(" has ");
-						Put(StackSize(S.Item));
-						Put(" integers.");
+						displaySize(S);
 					when others =>
-						Put("Not a valid option, please enter again.");
+						Put("[!] Not a valid option, please enter again.");
 				end case;
+				option := 6;
 			end loop WorkerLoop;
 		end loop BossLoop;
 	end stackMenu;
+
 	procedure listNames is
 	begin
 		for I in 1..5 loop
@@ -107,27 +130,35 @@ procedure Stacker is
 		end loop;
 	end listNames;
 
+	function verifyName (Name : Character) return Integer is
+	begin
+		for I in 1..5 loop
+			if ar(I).Name = Name then
+				return I;
+			end if;
+		end loop;
+		raise CONSTRAINT_ERROR;
+	end verifyName;
 	option : Character;
+	ArPos : Positive;
 begin
 	BossLoop:
 	loop
 		WorkerLoop:
 		loop
 			Put_Line("Which stack would you like to work with today? (0 to exit)");
+			Put_Line("Enter a space to use the first unused stack.");
 			listNames;
 			Put("Your selection: ");
-			declare
-				input : String(1..80);
-				length : Natural;
 			begin
-				Get_Line(input, length);
-				option := input(1);
+				Get(option); -- this really needs to be a Get_Line
+				exit BossLoop when option = '0';
+				ArPos := verifyName(option);
 			exception
-			-- dont need
 				when CONSTRAINT_ERROR => exit WorkerLoop;
 			end;
-			exit BossLoop when option = '0';
-			New_Line; Put("POO");
+			New_Line;
+			stackMenu(ar(ArPos));
 		end loop WorkerLoop;
 	end loop BossLoop;
 end Stacker;
